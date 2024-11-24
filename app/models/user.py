9bedae14cf6dfa56b569c49308590240d1b4b195
Avis Fung <avisf.avi@gmail.com>
2024-11-24 18:52:39 +0000

    fixed syntax

--- app/models/user.py
+++ app/models/user.py
@@ -4,12 +4,12 @@ from database.db import db
 
 class Users(db.Model):
     __tablename__ = "users"
-    extend_existing=True 
+    
     # columns 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(40))
-    username = db.Column(db.String(20))
-    password = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True, extend_existing=True)
+    email = db.Column(db.String(40), extend_existing=True)
+    username = db.Column(db.String(20), extend_existing=True)
+    password = db.Column(db.String(255), extend_existing=True)
     
     def __init__(self, email, username, password) -> None:
         self.email = email
